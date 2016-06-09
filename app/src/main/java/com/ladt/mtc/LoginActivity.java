51603ad76d967b83b23499a355a56b1c4e85249f
Laurent Nguyen <tplaurent@gmail.com>
2016-06-09 21:21:07 +0200

    MARCHE PAS DO IN BACKGROUND

--- app/src/main/java/com/ladt/mtc/LoginActivity.java
+++ app/src/main/java/com/ladt/mtc/LoginActivity.java
@@ -1,5 +1,7 @@
 package com.ladt.mtc;
 
+import android.os.AsyncTask;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.app.ProgressDialog;
 import android.os.Bundle;
@@ -48,17 +50,43 @@ public class LoginActivity extends AppCompatActivity {
     @BindView(R.id.link_signup)
     TextView _signupLink;
 
+   /* StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+
+    StrictMode.setThreadPolicy(policy); */
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
 
+        Toast.makeText(getBaseContext(), "CREATION", Toast.LENGTH_LONG).show();
+
         _loginButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                login();
+                new myTask().execute();
+                /*Thread thread = new Thread(new Runnable()
+                {
+                    @Override
+                    public void run()
+                    {
+                        try
+                        {
+                            login();
+                        }
+                        catch (Exception e)
+                        {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+
+                thread.start();*/
+
+
+                //login();
             }
         });
 
@@ -72,9 +100,142 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
     }
+     private class myTask extends AsyncTask<Void, Void, String>{
+        @Override
+        protected String doInBackground(Void... params){
+            Toast.makeText(getBaseContext(), "DO IN BACKGROUND", Toast.LENGTH_LONG).show();
+            //login();
+            Toast.makeText(getBaseContext(), "BRAVO VOUS AVEZ CLIQUE", Toast.LENGTH_LONG).show();
 
+            if (!validate()) {
+                onLoginFailed();
+            }
+
+            _loginButton.setEnabled(false);
+
+        /*final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this,
+                R.style.AppTheme);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Authenticating...");
+        progressDialog.show();*/
+
+            String username = _usernameText.getText().toString();
+            String password = _passwordText.getText().toString();
+
+            DefaultHttpClient mHttpClient = new DefaultHttpClient();
+            HttpPost mPost = new HttpPost("http://10.43.6.254:8888/web/php/login.php");
+
+            List<BasicNameValuePair> pairs = new ArrayList<BasicNameValuePair>();
+            pairs.add(new BasicNameValuePair("username", username));
+            pairs.add(new BasicNameValuePair("password", password));
+
+            try {
+                mPost.setEntity(new UrlEncodedFormEntity(pairs, HTTP.UTF_8));
+            } catch (UnsupportedEncodingException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+            try {
+                HttpResponse response = mHttpClient.execute(mPost);
+                int res = response.getStatusLine().getStatusCode();
+
+                if (res == 200) {
+                    HttpEntity entity = response.getEntity();
+
+                    if (entity != null) {
+                        String info = EntityUtils.toString(entity);
+                        System.out.println("info-----------"+info);
+                        //以下主要是对服务器端返回的数据进行解析
+
+                        JSONObject jsonObject=null;
+                        //flag为登录成功与否的标记,从服务器端返回的数据
+                        String flag="";
+                        String name="";
+                        String userid="";
+                        String sessionid="";
+                        try {
+                            jsonObject = new JSONObject(info);
+                            flag = jsonObject.getString("flag");
+                            name = jsonObject.getString("name");
+                            userid = jsonObject.getString("userid");
+                            sessionid = jsonObject.getString("sessionid");
+
+                        } catch (JSONException e) {
+                            // TODO Auto-generated catch block
+                            e.printStackTrace();
+                        }
+                        //根据服务器端返回的标记,判断服务端端验证是否成功
+
+                        if(flag.equals("success")){
+                            //为session传递相应的值,用于在session过程中记录相关用户信息
+                            session.put("s_userid", userid);
+                            session.put("s_username", name);
+                            session.put("s_sessionid", sessionid);
+                            onLoginSuccess();
+                            setUsername(username);
+                        }
+                        else{
+                            onLoginFailed();
+                        }
+                    }
+                    else{
+
+                        onLoginFailed();
+                    }
+
+                }
+
+            } catch (ClientProtocolException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+            // if (username.equals("admin")&&password.equals("1234")) {
+            //  Toast.makeText(getApplicationContext(), "Redirecting...",Toast.LENGTH_SHORT).show();
+            //     onLoginSuccess();
+            //     setUsername(username);
+            // }else {
+            //     onLoginFailed();
+            // }
+
+            new android.os.Handler().postDelayed(
+                    new Runnable() {
+                        public void run() {
+                            final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this,
+                                    R.style.AppTheme);
+                            // On complete call either onLoginSuccess or onLoginFailed
+                            onLoginSuccess();
+                            // onLoginFailed();
+                            progressDialog.dismiss();
+                        }
+                    }, 3000);
+
+            return "OK";
+        }
+         @Override
+         protected void onProgressUpdate(Void... progress){
+             super.onProgressUpdate();
+             final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this,
+                     R.style.AppTheme);
+             progressDialog.setIndeterminate(true);
+             progressDialog.setMessage("Authenticating...");
+             progressDialog.show();
+         }
+
+         /*@Override
+         protected void onPostExecute(String result){
+             super.onPostExecute(result);
+         }*/
+    }
+/*
     public void login() {
-        Log.d(TAG, "Login");
+        //Log.d(TAG, "Login");
+
+        Toast.makeText(getBaseContext(), "BRAVO VOUS AVEZ CLIQUE", Toast.LENGTH_LONG).show();
 
         if (!validate()) {
             onLoginFailed();
@@ -82,13 +243,13 @@ public class LoginActivity extends AppCompatActivity {
 
         _loginButton.setEnabled(false);
 
-        final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this,
+        /*final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this,
                 R.style.AppTheme);
         progressDialog.setIndeterminate(true);
         progressDialog.setMessage("Authenticating...");
-        progressDialog.show();
+        progressDialog.show();*/
 
-        String username = _usernameText.getText().toString();
+       /* String username = _usernameText.getText().toString();
         String password = _passwordText.getText().toString();
 
         DefaultHttpClient mHttpClient = new DefaultHttpClient();
@@ -174,6 +335,8 @@ public class LoginActivity extends AppCompatActivity {
         new android.os.Handler().postDelayed(
                 new Runnable() {
                     public void run() {
+                        final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this,
+                                R.style.AppTheme);
                         // On complete call either onLoginSuccess or onLoginFailed
                         onLoginSuccess();
                         // onLoginFailed();
@@ -181,7 +344,7 @@ public class LoginActivity extends AppCompatActivity {
                     }
                 }, 3000);
     }
-
+*/
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_SIGNUP) {
